87a2162abafc70953150993a1d6ac58fa65c3f3a
unknown <prajwalrao0404@gmail.com>
2024-08-27 13:50:37 +0530

    testing script updated

--- main_test.go
+++ main_test.go
@@ -58,31 +58,6 @@ func TestStaticCSSFile(t *testing.T) {
 	}
 }
 
-func TestStaticJSFile(t *testing.T) {
-	// Test serving a JS file
-	req, err := http.NewRequest("GET", "/js/script.js", nil)
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	rr := httptest.NewRecorder()
-	handler := http.FileServer(http.Dir("./static"))
-
-	handler.ServeHTTP(rr, req)
-
-	if status := rr.Code; status != http.StatusOK {
-		t.Errorf("handler returned wrong status code: got %v want %v",
-			status, http.StatusOK)
-	}
-
-	// Verify the content type for JS
-	expected := "application/javascript; charset=utf-8"
-	if contentType := rr.Header().Get("Content-Type"); contentType != expected {
-		t.Errorf("handler returned unexpected content type: got %v want %v",
-			contentType, expected)
-	}
-}
-
 func TestStaticImageFile(t *testing.T) {
 	// Test serving an image file
 	req, err := http.NewRequest("GET", "/images/logo.png", nil)
@@ -105,5 +80,43 @@ func TestStaticImageFile(t *testing.T) {
 	if contentType := rr.Header().Get("Content-Type"); contentType != expected {
 		t.Errorf("handler returned unexpected content type: got %v want %v",
 			contentType, expected)
+	}
+}
+
+func TestStaticJSFile(t *testing.T) {
+	// Test serving the JS file located at /js/site.js
+	req, err := http.NewRequest("GET", "/js/site.js", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	rr := httptest.NewRecorder()
+	handler := http.FileServer(http.Dir("./static"))
+
+	handler.ServeHTTP(rr, req)
+
+	if status := rr.Code; status != http.StatusOK {
+		t.Errorf("handler returned wrong status code: got %v want %v",
+			status, http.StatusOK)
+	}
+
+	// Accept both 'text/javascript' and 'application/javascript' as valid content types
+	expectedContentTypes := []string{
+		"text/javascript; charset=utf-8",
+		"application/javascript; charset=utf-8",
+	}
+
+	contentType := rr.Header().Get("Content-Type")
+	valid := false
+	for _, expected := range expectedContentTypes {
+		if contentType == expected {
+			valid = true
+			break
+		}
+	}
+
+	if !valid {
+		t.Errorf("handler returned unexpected content type: got %v want %v or %v",
+			contentType, expectedContentTypes[0], expectedContentTypes[1])
 	}
 }
